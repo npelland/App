c2869a6aaacb9b9f22ef98e8df0451317ec6bdfa
BuildTools <unconfigured@null.spigotmc.org>
2016-03-24 20:02:28 -0500

    Startted creating views

---
+++ app/AccountViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AccountViewController.swift
+//  app
+//
+//  Created by Nick Pelland on 3/24/16.
+//  Copyright © 2016 adamshurson. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
