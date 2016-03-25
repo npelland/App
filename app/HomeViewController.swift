b0e731036dab112eb3bafcb47245b99f353e7b41
BuildTools <unconfigured@null.spigotmc.org>
2016-03-24 22:42:50 -0500

    I cleaned things up, looks better on phone if leave everything same background color..I cant get search bar to get rid of small border...added close keyboard on tap

--- app/HomeViewController.swift
+++ app/HomeViewController.swift
@@ -13,11 +13,22 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(HomeViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
 
