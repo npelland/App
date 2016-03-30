582e88f0c60f463ea4ee9507e28e254dbda2f5d7
BuildTools <unconfigured@null.spigotmc.org>
2016-03-29 22:22:45 -0500

    Fixed white status bar

--- app/AppDelegate.swift
+++ app/AppDelegate.swift
@@ -17,9 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        
-        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
-        
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
         return true
     }
 
